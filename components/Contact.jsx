781dbb1d5fe32432e9b6a178e4320a2c926a0b55
abed2k4 <abed2k4@gmail.com>
2023-04-19 07:53:45 +0300

    a

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -27,8 +27,8 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className='py-2'>Clint Briley</h2>
-                <p>Front-End Developer</p>
+                <h2 className='py-2'>Abdul Rahman Malak</h2>
+                <p>Software Developer</p>
                 <p className='py-4'>
                   I am available for freelance or full-time positions. Contact
                   me and let&apos;s talk.
@@ -75,7 +75,7 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://getform.io/f/b221fbce-e6a1-43db-b62e-c35393f45e6d'
                 method='POST'
                 encType='multipart/form-data'
               >
